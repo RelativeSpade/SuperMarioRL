ead70f76d95358d17204f4b80c93ab285f487d5a
IAmSpade <34572462+RelativeSpade@users.noreply.github.com>
2024-06-30 04:52:25 -0400

    git ignore

--- venv/RL/SuperMarioRL.py
+++ venv/RL/SuperMarioRL.py
@@ -3,22 +3,40 @@ import gym_super_mario_bros
 from nes_py.wrappers import JoypadSpace
 from gym_super_mario_bros.actions import SIMPLE_MOVEMENT
 
-# Setup game
-env = gym_super_mario_bros.make('SuperMarioBros-v0')
-env = JoypadSpace(env, SIMPLE_MOVEMENT)
+#env = gym_super_mario_bros.make('SuperMarioBros-v0')
+#env = JoypadSpace(env, SIMPLE_MOVEMENT)
 
 # Create a flag whether done
-done = True
+#done = True
 
 # Loop through each frame in the game
-for step in range(10000):
+#for step in range(10000):
     # Start the game to begin
-    if done:
-        state = env.reset()
+    #if done:
+        #env.reset()
     # Random Move
-    state, reward, done, info = env.step(env.action_space.sample())
+    #state, reward, done, info = env.step(env.action_space.sample())
     # Update Screen
-    env.render()
+    #env.render()
 
 # Close when finished.
-env.close()
+#env.close()
+
+# Preprocess Enviorment
+
+# Import Frame Stacker Wrapper and GrayScaleing Wrapper
+from gym.wrappers import GrayScaleObservation
+# Import Vectorization Wrappers
+from stable_baselines3.common.vec_env import VecFrameStack, DummyVecEnv
+# Import matplotlib
+from matplotlib import pyplot as plt
+
+# 1. Create Base Enviorment
+env = gym_super_mario_bros.make('SuperMarioBros-v0')
+# 2. Simplify Controls
+env = JoypadSpace(env, SIMPLE_MOVEMENT)
+# 3. Grayscale
+env = GrayScaleObservation(env, keep_dim=True)
+# 4. Wrap inside the Dummy Enviorment
+
+# 5. Stack the Frames.
